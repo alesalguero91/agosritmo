9d9ac52c70cb87a593401fc170b0e0637eea802e
Alejandro <nestorale.salguero91@outlook.es>
2025-07-10 12:39:03 -0300

    mod urls in app

--- lectorpdf/urls.py
+++ lectorpdf/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('upload-pdf/', PDFUploadView.as_view(), name='upload-pdf'),
     path('generar-nota/', NotaGeneradaView.as_view(), name='generar-nota'),
  #   path('generar-nota-word/', NotaGeneradaViewEnWord.as_view(), name="generar-word") 
-    path('', subir_archivo_view, name='subir_archivo'),
+    path('formulario/', subir_archivo_view, name='subir_archivo'),
     path('procesar-excel/', ExcelUploadView.as_view(), name='procesar excel')
   
 ]
