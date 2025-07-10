943b740a3ff8b7a3fc35b63d9cb4355d944ae701
Alejandro <nestorale.salguero91@outlook.es>
2025-07-10 13:13:50 -0300

    neuva base

--- titanio/settings.py
+++ titanio/settings.py
@@ -96,14 +96,11 @@ WSGI_APPLICATION = 'titanio.wsgi.application'
 #        },
 #    }
 #}
-
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-#}
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.dummy',
+    }
+}
 
 
 
